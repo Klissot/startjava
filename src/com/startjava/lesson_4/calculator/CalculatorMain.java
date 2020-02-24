31ef06331d4415fc4518b71c9d77a9f778f4a9da
Klissot <klissot@gmail.com>
2020-02-25 01:54:54 +0600

    ok

--- src/com/startjava/lesson_4/calculator/CalculatorMain.java
+++ src/com/startjava/lesson_4/calculator/CalculatorMain.java
@@ -5,22 +5,18 @@ import java.util.Scanner;
 public class CalculatorMain {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
-        System.out.print("Введите математическое выражение: ");
-        String operation = scan.nextLine();
-        String[] strings = operation.split(" ");
         Calculator calc = new Calculator();
-        calc.setFirstValue(Integer.parseInt(strings[0]));
-        calc.setSecondValue(Integer.parseInt(strings[2]));
-        calc.setOperations(strings[1]);
 
         String answer;
         do {
-            calc.calculate();
+            System.out.print("Введите математическое выражение: ");
+            String operation = scan.nextLine();
+            String[] mathOperation = operation.split(" ");
+            calc.calculate(mathOperation);
             do {
-                System.out.println("Would you like to continue?");
-                answer = scan.next();
-            } while (!(answer.equals("Yes") || answer.equals("No")));
-        } while (!answer.equals("No"));
+                System.out.println("Хотите продолжить? [Да / Нет]");
+                answer = scan.nextLine();
+            } while(!(answer.equals("Да") || answer.equals("Нет")));
+        } while(!answer.equals("Нет"));
     }
 }
